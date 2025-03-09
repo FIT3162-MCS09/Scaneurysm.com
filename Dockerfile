6bc602d0d24d8adcd5616c0c9120d5f5dd678260
smal0039 <smal0039@student.monash.edu>
2025-03-09 13:47:24 +0800

    Add initial project structure with Docker, GitHub Actions, and React components

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM ubuntu:latest
+LABEL authors="malcolmpaltiraja"
+
+ENTRYPOINT ["top", "-b"]
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => (
+    <header>
+        <h1>Brain Aneurysm Detection & Explanation System</h1>
+    </header>
+);
+
+export default Header;
