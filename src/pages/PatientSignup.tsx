e1df6b9963c442f2f1802e1c8504c4a1497dcee6
smal0039 <smal0039@student.monash.edu>
2025-05-12 20:41:19 +0800

    Refactor button icon rendering to use React.createElement for consistency

--- src/pages/PatientSignup.tsx
+++ src/pages/PatientSignup.tsx
@@ -199,7 +199,7 @@ const PatientSignup = () => {
                   }}
                   style={{ width: '20%', minWidth: '40px' }}
               >
-                <FaSearch size={16} />
+                {React.createElement(FaSearch, { size: 16 })}
               </button>
             </div>
             {form.primary_doctor && (
